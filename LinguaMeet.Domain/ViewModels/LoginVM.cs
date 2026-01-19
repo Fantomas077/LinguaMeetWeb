0b26038425337784058cf715fef9ef9b9f25ea87
Fantomas077 <nheltonn@gmail.com>
2026-01-19 17:32:56 +0100

    AddViewNodel Login and Register

---
+++ LinguaMeet.Domain/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LinguaMeet.Domain.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string EmailAdresse { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ LinguaMeet.Domain/ViewModels/RegisterVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LinguaMeet.Domain.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
